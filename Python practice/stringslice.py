3e1aa7565e03b7eed8e217a966e7e7b37f53e92a
krina1902 <krinaantala33@gmail.com>
2023-10-11 21:36:18 +0530

    py

---
+++ Python practice/stringslice.py
@@ -0,0 +1,8 @@
+#   012345678901234567
+s= "Python programming"
+#   876543210987654321
+print(s[2:16:3])#tnrrr
+print(s[5:])#n programming
+print(s[:12])#Python progr
+print(s[3:13])#hon progra
+print(s[3:])#hon programming
--- Python practice/tempCodeRunnerFile.py
+++ Python practice/tempCodeRunnerFile.py
@@ -1,13 +1,35 @@
-# n=int(input("enter N:"))
-# sum=0
-# for i in range(1,n+1):
-#     sum=sum+i
-# print(sum)
+s="Krina Antala"
 
-n=int(input("enter N:"))
-sum=0
+print(s.count("a"))
+print(s.upper())
+print(s.casefold())
+print(s.center(20,"*"))
+print(s.lower())
+print(s.endswith("ala"))
+print(s.startswith("kri"))
+a=s.find("a A")
+print(a)
+print(s.index("a",11))
+print(s.isalnum()) #false
+print("k123".isalnum())
+print(s.isalpha()) #false
+print("k".isalpha()) 
+print("123".isnumeric()) 
+print("k123".isnumeric()) #false
+print("k 123".isnumeric()) #false
+print(s.isspace())#false
+print("  ".isspace())
+print(s.istitle())
+print(s.islower())#false
+print(s.isupper())#false
+print(s.swapcase())
+print(s.replace("a","l"))
+print(s.encode())
+print(s.expandtabs())
+print(s.isascii())
+print(s.join("Thumar"))
+print(s.ljust(3))
+print(s.rstrip("tala"))
+print(s.lstrip("Kri"))
 
-while n>0:
-    sum=sum+n
-    n=n+1
-print(sum)
+
