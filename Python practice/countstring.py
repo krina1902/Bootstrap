eadaa78964d8c16479026dc6e35e8805bc97efba
krina1902 <krinaantala33@gmail.com>
2023-10-13 17:57:25 +0530

    py

---
+++ Python practice/countstring.py
@@ -0,0 +1,23 @@
+s=input("Enter String")
+
+lower=0
+upper=0
+space=0
+char=0
+num=0
+for i in s:
+    if i.isupper():
+        upper+=1
+    elif i.islower():
+        lower+=1
+    elif i.isspace():
+        space+=1
+    elif i.isalpha():
+        char+=1
+    elif i.isnumeric():
+        num+=1
+print("Total upper: ",upper)
+print("Total lower: ",lower)
+print("Total space: ",space)
+print("Total char: ",char)
+print("Total num: ",num)
--- Python practice/stringslice.py
+++ Python practice/stringslice.py
@@ -1,8 +1,16 @@
 #   012345678901234567
 s= "Python programming"
 #   876543210987654321
-print(s[2:16:3])#tnrrr
+print(s[::-1])
+print(s[-14:-4:4])#ora
+print(s[-13:-6:-4])#
+print(s[-6:-13:-4])#ar
+print(s[-17:])#ython programming
+print(s[-15:-3])#hon programm
+print(s[::5])#Pngi
+print(s[2:16:3])#tnrrm
 print(s[5:])#n programming
 print(s[:12])#Python progr
 print(s[3:13])#hon progra
 print(s[3:])#hon programming
+print(s[:-5])#Python progra
